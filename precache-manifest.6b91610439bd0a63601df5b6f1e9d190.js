bd17b249507fb5c34532c96afa240a6f2e2405e3
Kamil Szewczyk <kamilszewczyk0@gmail.com>
2020-09-05 01:31:16 +0200

    Updates

---
+++ precache-manifest.6b91610439bd0a63601df5b6f1e9d190.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d61f7f54d7c8956370073f1efbf4886",
+    "url": "/Currency_Converter--React/index.html"
+  },
+  {
+    "revision": "a239100585c26f0852ee",
+    "url": "/Currency_Converter--React/static/css/main.c6da4def.chunk.css"
+  },
+  {
+    "revision": "e85e677c595a7fab33e8",
+    "url": "/Currency_Converter--React/static/js/2.408752a3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Currency_Converter--React/static/js/2.408752a3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a239100585c26f0852ee",
+    "url": "/Currency_Converter--React/static/js/main.9012a786.chunk.js"
+  },
+  {
+    "revision": "6e560656fc3f7ed398bd",
+    "url": "/Currency_Converter--React/static/js/runtime-main.46978a16.js"
+  }
+]);
