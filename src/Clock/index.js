6384a09fc342ac7489e6f260270c5b2815f0db62
Kamil Szewczyk <kamilszewczyk0@gmail.com>
2020-09-10 01:43:18 +0200

    fix time format

--- src/Clock/index.js
+++ src/Clock/index.js
@@ -3,11 +3,14 @@ import "./style.css";
 import {useCurrentDate} from "./useCurrentDate";
 
 const dateFormat = (date) =>
-  date.toLocaleString("en", {
+  date.toLocaleString("en-GB", {
     weekday: "long",
     month: "long",
     day: "numeric",
     year: "numeric",
+    hour: "numeric",
+    minute: "numeric",
+    second: "numeric",
   });
 
 const Clock = () => {
